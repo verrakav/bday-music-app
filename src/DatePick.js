f203fb0610f62c91bc9d337d9e3370b6d9b43f81
verrakav <verrakav@users.noreply.github.com>
2023-12-05 21:46:58 +1100

    Dec5.1
    tried changing the calendar using imported DatePicker - like with tags more

--- src/DatePick.js
+++ src/DatePick.js
@@ -1,11 +1,28 @@
-import React, from "react";
-import DatePicker form 'react-datepicker';
+import React, { useState } from "react";
+import "react-datepicker/dist/react-datepicker.css"; /*will it work without? how?*/
+
+import DatePicker from "react-datepicker";
 import ButtonSubmit from "./ButtonSubmit";
 
 function DatePick() {
+  const [date, setDate] = useState(null);
+  const handleDateChange = (date) => setDate(date);
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="input-container">
-      <input type="date" name="bday" className="input-date" />
+      {/* below is the imported component */}
+      <DatePicker
+        selected={date}
+        onChange={handleDateChange}
+        className="input-date"
+        placeholderText="day/month/year"
+        // the stuff below limits the year selection
+        showYearDropdown={true}
+        yearDropdownItemNumber={100}
+        minDate={new Date("1940-01-01")}
+        maxDate={new Date(currentYear)}
+      />
       <ButtonSubmit />
     </div>
   );
