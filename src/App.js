8af61f9ec484034d6f96736cb35390412e187cb5
verrakav <verrakav@users.noreply.github.com>
2024-04-06 17:27:29 +1100

    a fresh start

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import React from "react";
-import "./App.css";
-import ColumnWrapper from "./ColumnWrapper";
-
 function App() {
-  return (
-    <div className="App">
-      <h1 className="heading">Songs from the Top</h1>
-      <ColumnWrapper />
-    </div>
-  );
+  return <div className="App">app</div>;
 }
 
 export default App;
