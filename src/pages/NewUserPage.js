0f3537f50a352d1d56e5ebfa950ab845112075b5
verrakav <verrakav@users.noreply.github.com>
2024-08-19 13:15:26 +1000

    starting off with navigation

--- src/pages/NewUserPage.js
+++ src/pages/NewUserPage.js
@@ -5,17 +5,9 @@ import DateList from "../components/DateList";
 function NewUserPage() {
   const [selectedDate, setSelectedDate] = useState(null);
 
-  const handleDateChange = () => {
-    console.log(selectedDate.$d);
-  };
-
   return (
     <div className="align-center flex flex-col items-center">
-      <DatePickerCombined
-        onDateSubmit={handleDateChange}
-        setSelectedDate={setSelectedDate}
-        selectedDate={selectedDate}
-      />
+      <DatePickerCombined />
       <DateList className="flex" />
     </div>
   );
