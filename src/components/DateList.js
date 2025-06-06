1a3d82f9fd4002eaf3af08e56f4e6fcdfaaf9c27
verrakav <verrafalenko@gmail.com>
2025-06-06 13:54:40 +0930

    starting over... again

--- src/components/DateList.js
+++
@@ -1,9 +0,0 @@
-import {useEffect, useState} from "react";
-
-export default function DateList({selectedDate}) {
-  const [dateList, setDateList] = useState([]);
-  useEffect(() => {
-    setDateList(selectedDate);
-  }, []);
-  return <div>{dateList}</div>;
-}
--- src/pages/ExistingUser.js
+++
@@ -1,7 +0,0 @@
-export default function ExistingUser() {
-  return (
-    <div className="flex flex-column justify-center m-5">
-      Existing user page
-    </div>
-  );
-}
