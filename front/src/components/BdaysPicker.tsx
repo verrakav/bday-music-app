e1b87f38b97794a931442e0eefe1501eb794fb0d
verrakav <verrafalenko@gmail.com>
2025-06-08 12:16:31 +0930

    connecting the front and the back

--- front/src/components/BdaysPicker.tsx
+++ front/src/components/BdaysPicker.tsx
@@ -19,20 +19,35 @@ export default function BdaysPicker() {
     if (bdays.length === 5) setMessage("Enough bdays!");
   };
 
+  // clears the message when the list shrinks
   useEffect(() => {
     if (bdays.length < 5) {
       setMessage("");
     }
   }, [bdays]);
+
   // removes a date if not needed
   const handleRemoveDate = (dateIdx: number) => {
     const updatedDates = bdays.filter((_, idx) => idx !== dateIdx);
 
     setBdays(updatedDates);
   };
+
+  //  NEW:
+  // const fetchASong = async (date) => {
+  const fetchASong = async () => {
+    const date = "2000-09-24";
+    console.log("FETCHING DATA");
+    const res = await fetch(`http://localhost:3001/chart:${date}`, {
+      method: "GET",
+    });
+    const data = await res.json();
+    console.log("DATA", data);
+  };
+
   return (
     <div className="flex flex-col w-2/3 bg-green-300">
-      <p>NAME HERE</p>
+      <p>userNAME HERE</p>
       <input
         type="date"
         min="1950-01-01"
@@ -48,7 +63,14 @@ export default function BdaysPicker() {
           return (
             <li key={idx} className="flex flex-row justify-around">
               <p>{date}</p>
-              <button onClick={() => handleRemoveDate(idx)}>X</button>
+              <button
+                onClick={() => {
+                  fetchASong();
+                  // handleRemoveDate(idx);
+                }}
+              >
+                X
+              </button>
             </li>
           );
         })}
